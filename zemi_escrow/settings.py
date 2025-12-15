b2a11a8115f67046c60b26647e370e0d8e776d98
Petermartin064 <petermartin602@gmail.com>
2025-12-15 13:06:58 +0300

    Modeled the DB < Added Order, Payment, Payout & webhook models and also fixed the postgres DB error and switched to sqlite3 db for this MVP >

--- zemi_escrow/settings.py
+++ zemi_escrow/settings.py
@@ -55,12 +55,8 @@ WSGI_APPLICATION = 'zemi_escrow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'zemi_escrow'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
